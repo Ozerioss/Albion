142e27846fe321c66ec3183b84daddf45dc832d0
Karim Kidiss <ozerioss@gmail.com>
2021-06-17 13:24:14 +0200

    feat: add method to trigger an action

--- main.py
+++ main.py
@@ -2,26 +2,26 @@ import time
 from pathlib import Path
 
 import pyautogui
-import pyautogui as gui
 
 
-def position_test():
-    position = gui.position()
-    return position
-
-
-def locate_enemy_health_bar():
-    enemy_health_bar = Path("assets/health_bar_flagged.png", confidence=0.6)
+def locate_enemy_health_bar(enemy_health_bar: Path):
     while True:
         # TODO : optimize where to look on screen
         # TODO : maybe add a check on healthbar color ?
-        if pyautogui.locateOnScreen(str(enemy_health_bar)) is None:
+        if pyautogui.locateOnScreen(str(enemy_health_bar), confidence=0.7) is None:
             print("No enemy nearby")
             time.sleep(0.5)
         else:
             print("ENEMY SPOTTED")
+            run_action("r")
             time.sleep(1)
+            break
+
+
+def run_action(action: str):
+    pyautogui.press(action)
+    print(f"Pressed {action}")
 
 
 if __name__ == "__main__":
-    locate_enemy_health_bar()
+    locate_enemy_health_bar(Path("assets/health_bar_flagged.png"))
