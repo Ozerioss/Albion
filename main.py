9c9282b4a87ac30212bda72b22f4f3698c7c6d7d
Karim Kidiss <ozerioss@gmail.com>
2021-06-17 15:36:29 +0200

    feat: add grayscale option to handle low opacity health bars

--- main.py
+++ main.py
@@ -8,13 +8,12 @@ def locate_enemy_health_bar(enemy_health_bar: Path):
     while True:
         # TODO : optimize where to look on screen
         # TODO : maybe add a check on healthbar color ?
-        if pyautogui.locateOnScreen(str(enemy_health_bar), confidence=0.7) is None:
+        if pyautogui.locateOnScreen(str(enemy_health_bar), confidence=0.79, grayscale=True) is None:
             print("No enemy nearby")
-            time.sleep(0.5)
+            time.sleep(0.2)
         else:
             print("ENEMY SPOTTED")
             run_action("r")
-            time.sleep(1)
             break
 
 
