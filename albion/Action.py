7eeba41b191a75b7a05a097bd7dea5e6dd3e1407
Karim Kidiss <ozerioss@gmail.com>
2021-06-17 19:18:11 +0200

    feat: add ability to stop the script on key press & refactor project layout

--- albion/Action.py
+++ albion/Action.py
@@ -4,3 +4,4 @@ from enum import Enum
 class Action(str, Enum):
     chest_ability = "R"
     potion = "é"
+    stop_script = "ù"
--- albion/main.py
+++ albion/main.py
@@ -1,16 +1,23 @@
 import time
 from pathlib import Path
-from Action import Action
 
+import keyboard
 import pyautogui
 
+from Action import Action
 
-def locate_enemy_health_bar(enemy_health_bar: Path):
+
+def locate_enemy_health_bar(enemy_health_bar: Path, confidence: float = 0.9):
     # TODO : optimize where to look on screen
     # TODO : maybe add a check on healthbar color ?
-    if pyautogui.locateOnScreen(str(enemy_health_bar), confidence=0.778, grayscale=False) is None:
+    if (
+        pyautogui.locateOnScreen(
+            str(enemy_health_bar), confidence=confidence, grayscale=False
+        )
+        is None
+    ):
         print("No enemy nearby")
-        time.sleep(0.2)
+        time.sleep(0.3)
         return False
     else:
         print("ENEMY SPOTTED")
@@ -24,11 +31,12 @@ def run_action(action: str):
 
 
 if __name__ == "__main__":
-    # TODO break out of the loop on key press
     while True:
-        is_located = locate_enemy_health_bar(Path("assets/health_bar_low_opacity.png"))
+        if keyboard.is_pressed(Action.stop_script):
+            print("Stopping script")
+            break
+        is_located = locate_enemy_health_bar(
+            Path("assets/health_bar_low_opacity.png"), confidence=0.778
+        )
         if is_located:
             run_action(Action.chest_ability)
-            break
-
-
