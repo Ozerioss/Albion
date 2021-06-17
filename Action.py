cbbab80c82ac0b0b2c45e863795caff84a2a832b
Karim Kidiss <ozerioss@gmail.com>
2021-06-17 18:15:19 +0200

    refactor: moves the logic for better reusability of methods

---
+++ Action.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class Action(str, Enum):
+    chest_ability = "R"
+    potion = "é"
--- main.py
+++ main.py
@@ -1,26 +1,34 @@
 import time
 from pathlib import Path
+from Action import Action
 
 import pyautogui
 
 
 def locate_enemy_health_bar(enemy_health_bar: Path):
-    while True:
-        # TODO : optimize where to look on screen
-        # TODO : maybe add a check on healthbar color ?
-        if pyautogui.locateOnScreen(str(enemy_health_bar), confidence=0.778, grayscale=False) is None:
-            print("No enemy nearby")
-            time.sleep(0.2)
-        else:
-            print("ENEMY SPOTTED")
-            run_action("r")
-            break
+    # TODO : optimize where to look on screen
+    # TODO : maybe add a check on healthbar color ?
+    if pyautogui.locateOnScreen(str(enemy_health_bar), confidence=0.778, grayscale=False) is None:
+        print("No enemy nearby")
+        time.sleep(0.2)
+        return False
+    else:
+        print("ENEMY SPOTTED")
+        return True
 
 
 def run_action(action: str):
-    pyautogui.press(action)
-    print(f"Pressed {action}")
+    if action:
+        pyautogui.press(action)
+        print(f"Pressed {Action[action]}, mapped to button {action}")
 
 
 if __name__ == "__main__":
-    locate_enemy_health_bar(Path("assets/health_bar_low_opacity.png"))
+    # TODO break out of the loop on key press
+    while True:
+        is_located = locate_enemy_health_bar(Path("assets/health_bar_low_opacity.png"))
+        if is_located:
+            run_action(Action.chest_ability)
+            break
+
+
