9c7d26ecd8a48b479ecda94766afb5f671c4b922
Karim Kidiss <ozerioss@gmail.com>
2022-04-26 21:37:48 +0200

    feat: add LUA script to auto sell

---
+++ albion/lua_scripts/sell_macro.lua
@@ -0,0 +1,52 @@
+function sellItems()
+  -- For debugging purposes
+  --x, y = GetMousePosition();
+  --OutputLogMessage("Mouse is at %d, %d\n", x, y);
+  sell()
+  undercut()
+  create_sell_order()
+end
+
+function sell()
+  moveNclick(43713, 26117)
+end
+
+function initSell()
+  sell()
+  moveNclick(19124, 31401) -- sell order only once per loop of full inventory
+  undercut()
+  create_sell_order()
+end
+
+function undercut()
+  moveNclick(19261, 38750)
+end
+
+function create_sell_order()
+  moveNclick(30394, 44034)
+end
+
+function moveNclick(x, y)
+  MoveMouseTo(x, y)
+  Sleep(800)
+  PressMouseButton(1)
+  Sleep(50)
+  ReleaseMouseButton(1)
+  Sleep(800)
+end
+
+function OnEvent(event, arg)
+    OutputLogMessage("Event: "..event.." Arg: "..arg.."\n")
+    if event == "MOUSE_BUTTON_PRESSED" and arg == 3
+    then
+      for item_index = 1, 48, 1
+      do
+        if item_index == 1
+        then
+          initSell()
+        end
+        OutputLogMessage("Selling item %d of 48\n", item_index)
+        sellItems()
+      end
+    end
+end
